ce4973bdd6faaffb371aaf53f2ed4679f3039576
kolikay <kolikay1989@gmail.com>
2022-06-22 17:12:18 +0100

    working on homepage screen2

--- lib/screens/home_screens/home_page.dart
+++ lib/screens/home_screens/home_page.dart
@@ -32,7 +32,7 @@ class _HomePageScreenState extends State<HomePageScreen> {
               Column(
                 children: [
                   const SizedBox(
-                    height: 20,
+                    height: 30,
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -56,7 +56,7 @@ class _HomePageScreenState extends State<HomePageScreen> {
                       // enlargeCenterPage: true,
                       viewportFraction: 0.9,
                       // autoPlay: true,
-
+        
                       onPageChanged: (index, reason) =>
                           setState(() => activeImageIndex = index),
                       autoPlayInterval: const Duration(seconds: 2),
@@ -70,15 +70,15 @@ class _HomePageScreenState extends State<HomePageScreen> {
                   const SizedBox(height: 10),
                   buildIndicator(),
                   const SizedBox(height: 10),
-                  const Divider(
-                    color: Colors.black26,
-                    thickness: 2.0,
-                  ),
-                  const SizedBox(height: 24),
                   Padding(
                     padding: const EdgeInsets.only(left: 24.0, right: 24.0),
                     child: Column(
                       children: [
+                        const Divider(
+                          color: Colors.black26,
+                          thickness: 2.0,
+                        ),
+                        const SizedBox(height: 20),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
@@ -97,12 +97,130 @@ class _HomePageScreenState extends State<HomePageScreen> {
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(10),
                           ),
-                          child: Column(
-                            children: [Row(
-                              children: [],
-                            ), Row(
-                              children: [],
-                            )],
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: GridView.count(
+                              primary: false,
+                              crossAxisSpacing: 1,
+                              mainAxisSpacing: 1,
+                              crossAxisCount: 4,
+                              children: <Widget>[
+                                newInkwell(context, 'Material',
+                                    'lib/assets/home1.png'),
+                                newInkwell(context, 'Past Question',
+                                    'lib/assets/home2.png'),
+                                newInkwell(context, 'Scheduler',
+                                    'lib/assets/home3.png'),
+                                newInkwell(context, 'Note Book',
+                                    'lib/assets/home4.png'),
+                                newInkwell(context, 'Scholarships',
+                                    'lib/assets/home5.png'),
+                                newInkwell(context, 'Notice Boards',
+                                    'lib/assets/home6.png'),
+                                newInkwell(
+                                    context, 'Chats', 'lib/assets/home7.png'),
+                                newInkwell(
+                                    context, 'More', 'lib/assets/home8.png'),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        const Divider(
+                          color: Colors.black26,
+                          thickness: 2.0,
+                        ),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            NormalText(
+                              text: 'Recent Activities',
+                              size: 19.2,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ],
+                        ),
+                        Container(
+                          width: 343,
+                          height: 83,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: Column(
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        NormalText(
+                                          text:
+                                              '2021/2011 Undergraduate students \nAccomodations for OAU Students',
+                                          size: 14,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                        Container(
+                                          height: 10,
+                                          width: 10,
+                                          decoration: const BoxDecoration(
+                                            color: Colors.blue,
+                                            shape: BoxShape.circle,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    NormalText(
+                                      text: '2 hours ago',
+                                      size: 12,
+                                      color: AppColor.dullBlack,
+                                    ),
+                                  ],
+                                ),
+                                // Column(
+                                //   crossAxisAlignment: CrossAxisAlignment.start,
+                                //   children: [
+                                //     Row(
+                                //       mainAxisAlignment:
+                                //           MainAxisAlignment.spaceBetween,
+                                //       children: [
+                                //         NormalText(
+                                //           text:
+                                //               '2021/2011 Undergraduate students \nAccomodations for OAU Students',
+                                //           size: 14,
+                                //           fontWeight: FontWeight.w500,
+                                //         ),
+                                //         Container(
+                                //           height: 10,
+                                //           width: 10,
+                                //           decoration: const BoxDecoration(
+                                //             color: Colors.blue,
+                                //             shape: BoxShape.circle,
+                                //           ),
+                                //         ),
+                                //       ],
+                                //     ),
+                                //     const SizedBox(
+                                //       height: 10,
+                                //     ),
+                                //     NormalText(
+                                //       text: '2 hours ago',
+                                //       size: 12,
+                                //       color: AppColor.dullBlack,
+                                //     ),
+                                //   ],
+                                // ),
+                              ],
+                            ),
                           ),
                         ),
                       ],
@@ -131,12 +249,45 @@ class _HomePageScreenState extends State<HomePageScreen> {
 
   Widget buildIndicator() {
     return AnimatedSmoothIndicator(
-        activeIndex: activeImageIndex,
-        count: images.length,
-        effect: const WormEffect(
-            dotWidth: 8.0,
-            dotHeight: 8.0,
-            activeDotColor: AppColor.dullBlack,
-            dotColor: Colors.black12));
+      activeIndex: activeImageIndex,
+      count: images.length,
+      effect: const WormEffect(
+          dotWidth: 8.0,
+          dotHeight: 8.0,
+          activeDotColor: AppColor.dullBlack,
+          dotColor: Colors.black12),
+    );
+  }
+
+  InkWell newInkwell(BuildContext context, String label, String imgUrl) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ),
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: ResizeImage(AssetImage(imgUrl), height: 30, width: 30),
+                ),
+              ),
+            ),
+            NormalText(
+              text: label,
+              size: 10,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
