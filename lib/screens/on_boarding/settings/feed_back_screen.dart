8697aa47c041ad77cedfb51d97f9d24b75476c00
kolikay <kolikay1989@gmail.com>
2022-07-15 17:19:11 +0100

    working on  feedback screen

--- lib/screens/on_boarding/settings/feed_back_screen.dart
+++ lib/screens/on_boarding/settings/feed_back_screen.dart
@@ -1,10 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'package:scholars_padi/constants/appColor.dart';
 import 'package:scholars_padi/widgets/reusesable_widget/normal_text.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:scholars_padi/widgets/reusesable_widget/reusaable_textformfield.dart';
+import 'package:scholars_padi/widgets/reusesable_widget/reuseable_button.dart';
 import 'package:scholars_padi/widgets/reusesable_widget/reusesable_appBar2.dart';
-
 
 class FeedBackScreen extends StatefulWidget {
   const FeedBackScreen({Key? key}) : super(key: key);
@@ -14,7 +14,6 @@ class FeedBackScreen extends StatefulWidget {
 }
 
 class _FeedBackScreenState extends State<FeedBackScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,10 +37,49 @@ class _FeedBackScreenState extends State<FeedBackScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 24.h,),
-              NormalText(text: 'Have Feedback? We would love to hear it, but please don\'t share sensitive information.', fontWeight: FontWeight.w500, size: 16,color: AppColor.dullBlack,),
-              NormalText(text: 'For technical support, please Contact Us', fontWeight: FontWeight.w500, size: 16,color: AppColor.dullBlack,),
-              NormalText(text: 'Please enter your feedback here', fontWeight: FontWeight.w500, size: 16,color: AppColor.dullBlack,),
+              SizedBox(
+                height: 24.h,
+              ),
+              NormalText(
+                text:
+                    'Have Feedback? We would love to hear it, but please don\'t share sensitive information.',
+                fontWeight: FontWeight.w500,
+                size: 16,
+                color: AppColor.dullBlack,
+              ),
+              NormalText(
+                text: 'For technical support, please Contact Us',
+                fontWeight: FontWeight.w500,
+                size: 16,
+                color: AppColor.dullBlack,
+              ),
+              NormalText(
+                text: 'Please enter your feedback here',
+                fontWeight: FontWeight.w500,
+                size: 16,
+                color: AppColor.dullBlack,
+              ),
+              SizedBox(
+                height: 24.h,
+              ),
+              TextField(
+                maxLength: 300,
+                decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1.0.w),
+                  ),
+               
+                ),
+              ),
+              SizedBox(
+                height: 24.h,
+              ),
+              ReuseableButton(
+                onPressed: () {},
+                text: 'Submit',
+              )
             ],
           ),
         ),
