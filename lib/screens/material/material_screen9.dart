cbaf87ed1308be3df96f26b36f49571b5324376f
kolikay <kolikay1989@gmail.com>
2022-07-28 16:22:42 +0100

    finished material screens

--- lib/screens/material/material_screen9.dart
+++ lib/screens/material/material_screen9.dart
@@ -17,134 +17,191 @@ class MaterialScreen9 extends StatefulWidget {
 
 class _MaterialScreen9State extends State<MaterialScreen9> {
   bool selected = false;
+  String message = 'Theory';
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(103.h),
-        child: ReuseableAppBar1(
-          title: NormalText(
-            text: widget.title,
-            color: Colors.white,
-            fontWeight: FontWeight.w500,
-          ),
-          firstAppIcon: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.arrow_back_ios),
-          ),
-          secondAppIcon: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.search),
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(103.h),
+          child: ReuseableAppBar1(
+            title: NormalText(
+              text: widget.title,
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            ),
+            firstAppIcon: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back_ios),
+            ),
+            secondAppIcon: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search),
+            ),
           ),
         ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.w),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 24.h,
-              ),
-              Container(
-                width: 310.w,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  border: Border.all(color: AppColor.mainColor),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.w),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 24.h,
                 ),
-                child: Row(
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          selected = !selected;
-                        });
-                      },
-                      child: Container(
-                        height: 50.h,
-                        width: 152.w,
-                        color: selected ? AppColor.mainColor : Colors.white,
-                        child: Center(
-                          child: NormalText(
-                            text: 'Objective',
-                            fontWeight: FontWeight.bold,
-                            color: selected ? Colors.white : Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: InkWell(
+                Container(
+                  width: 310.w,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(color: AppColor.mainColor),
+                  ),
+                  child: Row(
+                    children: [
+                      InkWell(
                         onTap: () {
                           setState(() {
                             selected = !selected;
+                            message = 'Objective';
                           });
                         },
                         child: Container(
                           height: 50.h,
                           width: 152.w,
-                          color: selected ? Colors.white : AppColor.mainColor,
+                          color: selected ? AppColor.mainColor : Colors.white,
                           child: Center(
                             child: NormalText(
-                              text: 'Theory',
+                              text: 'Objective',
                               fontWeight: FontWeight.bold,
-                              color: selected ? Colors.black : Colors.white,
+                              color: selected ? Colors.white : Colors.black,
                             ),
                           ),
                         ),
                       ),
-                    )
-                  ],
+                      Expanded(
+                        child: InkWell(
+                          onTap: () {
+                            setState(() {
+                              selected = !selected;
+                              message = 'Theory';
+                            });
+                          },
+                          child: Container(
+                            height: 50.h,
+                            width: 152.w,
+                            color: selected ? Colors.white : AppColor.mainColor,
+                            child: Center(
+                              child: NormalText(
+                                text: 'Theory',
+                                fontWeight: FontWeight.bold,
+                                color: selected ? Colors.black : Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ]),
+                SizedBox(
+                  height: 25.h,
+                ),
+                 MaterialCards(
+                  cardMessage: 'CHM 101 $message',
+                  cardDate: '2015',
+                  cardColor:const Color(0xffF65B3C),
+                ),
+                SizedBox(
+                  height: 15.h,
+                ),
+                MaterialCards(
+                  cardMessage: 'CHM 101 $message',
+                  cardDate: '2016',
+                  cardColor: Colors.blue,
+                ),
+                SizedBox(
+                  height: 15.h,
+                ),
+                 MaterialCards(
+                  cardMessage: 'CHM 101 $message',
+                  cardDate: '2017',
+                  cardColor: Colors.red,
+                ),
+                SizedBox(
+                  height: 15.h,
+                ),
+                 MaterialCards(
+                  cardMessage: 'CHM 101 $message',
+                  cardDate: '2018',
+                  cardColor: Colors.green,
+                ),
+                SizedBox(
+                  height: 15.h,
+                ),
+                MaterialCards(
+                  cardMessage: 'CHM 101 $message',
+                  cardDate: '2019',
+                  cardColor:const Color(0xffFFC847),
+                ),
+              ]),
+        ),
       ),
-    ));
+    );
   }
 }
 
-// class MaterialCards extends StatelessWidget {
-//   const MaterialCards({
-//     Key? key,
-//     required this.cardMessage,
-//   }) : super(key: key);
+class MaterialCards extends StatelessWidget {
+  const MaterialCards(
+      {Key? key,
+      required this.cardMessage,
+      required this.cardDate,
+      required this.cardColor})
+      : super(key: key);
 
-//   final String cardMessage;
+  final String cardMessage;
+  final String cardDate;
+  final Color cardColor;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return InkWell(
-//       onTap: () {
-    
-//       },
-//       child: Container(
-//         height: 50.h,
-//         decoration: BoxDecoration(
-//             borderRadius: BorderRadius.circular(5),
-//             border: Border.all(color: Colors.black12)),
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//           children: [
-//             Padding(
-//               padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
-//               child: NormalText(
-//                 text: cardMessage,
-//                 fontWeight: FontWeight.w500,
-//                 size: 16.sp,
-//                 color: Colors.black54,
-//               ),
-//             ),
-//             const Icon(
-//               Icons.arrow_forward_ios,
-//               color: Colors.black54,
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 327,
+      height: 50.h,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          border: Border.all(color: Colors.black12)),
+      child: Row(
+        children: [
+          Container(
+            height: 50.h,
+            width: 75,
+            color: cardColor,
+            child: Center(
+              child: NormalText(
+                text: cardDate,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                size: 18.sp,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.only(left: 10.0.w),
+                child: NormalText(
+                  text: cardMessage,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  size: 16.sp,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
