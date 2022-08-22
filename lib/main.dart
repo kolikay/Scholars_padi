aef1e854ab0f4e14388654a6855c002fff6b804b
kolikay <kolikay1989@gmail.com>
2022-08-22 17:21:07 +0100

    upgraded sdk

--- lib/main.dart
+++ lib/main.dart
@@ -27,13 +27,55 @@ void main() {
 //   runApp(const ProviderScope(child: MyApp()));
 // }
 
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ScreenUtilInit(
+//       builder: (context, child) {
+//         return MaterialApp(
+//           debugShowCheckedModeBanner: false,
+//           title: 'Skill Connect',
+//           theme: ThemeData(
+//             primarySwatch: Colors.blue,
+//           ),
+//            home: const LandingPage(),
+//         routes: {
+//           HomePageScreen.id: (context) => const HomePageScreen(),
+//           CatergoriesSearchScreen.id: (context) => const CatergoriesSearchScreen(),
+//           CategoriesPage.id: (context) => const CategoriesPage(),
+//            OnBoardingScreen.id: (context) => const OnBoardingScreen(),
+//            LoginScreen.id: (context) => const LoginScreen(),
+//            SettingsScreen.id: ((context) => const SettingsScreen()),
+//            ProfileScreen.id: ((context) => const ProfileScreen())
+//         },
+//         );
+//       },
+//       designSize: const Size(375, 812),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    //Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in,unit in dp)
     return ScreenUtilInit(
-      builder: () {
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context , child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Skill Connect',
@@ -50,9 +92,9 @@ class MyApp extends StatelessWidget {
            SettingsScreen.id: ((context) => const SettingsScreen()),
            ProfileScreen.id: ((context) => const ProfileScreen())
         },
-        );
+        ); 
       },
-      designSize: const Size(375, 812),
+      
     );
   }
 }
